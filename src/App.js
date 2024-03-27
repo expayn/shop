c8bb8564e02bdfe485919c87b4ec7ad95c4d176a
expayn <ascodev25@gmail.com>
2024-03-27 22:20:15 +0300

    #

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './component/Header'
+import Footer from './component/Footer'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
